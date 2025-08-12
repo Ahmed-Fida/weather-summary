53e480c3e1587ecc9c13bb4b99772b5949057e26
Ahmed Fida <ahmed.fida@A006-00973.local>
2025-08-12 12:10:31 +0500

    Refined the code

--- weather-summary.js
+++ weather-summary.js
@@ -1,48 +1,26 @@
-function average_humidity(weatherData, totalData) {
-  let average,
-    sum = 0;
-  for (let i = 0; i < totalData; i++) {
-    sum = sum + weatherData[i].humidity;
-  }
-  average = sum / totalData;
+function averageHumidity(weatherData, totalData) {
+  let sum = 0;
+  weatherData.forEach((item) => (sum += item.humidity));
+  let average = sum / totalData;
   return `Average humidity was ${average}%`;
 }
 
-function highest_temperature(weatherData, totalData) {
-  let highest_temperature_index = 0,
-    highest_temperature = weatherData[0].maxTemp;
-  for (let i = 1; i < totalData; i++) {
-    if (highest_temperature < weatherData[i].maxTemp) {
-      highest_temperature = weatherData[i].maxTemp;
-      highest_temperature_index = i;
-    }
-  }
-  return `Highest temperature was ${highest_temperature} on ${weatherData[highest_temperature_index].date} in city ${weatherData[highest_temperature_index].city}, ${weatherData[highest_temperature_index].country}.`;
+function highestTemperature(weatherData) {
+  let highest = weatherData.reduce((max, item) =>
+    item.maxTemp > max.maxTemp ? item : max
+  );
+  return `Highest temperature was ${highest.maxTemp} on ${highest.date} in city ${highest.city}, ${highest.country}.`;
 }
 
-function lowest_temperature(weatherData, totalData) {
-  let lowest_temperature_index = 0,
-    lowest_temperature = weatherData[0].minTemp;
-  for (let i = 1; i < totalData; i++) {
-    if (lowest_temperature > weatherData[i].minTemp) {
-      lowest_temperature = weatherData[i].minTemp;
-      lowest_temperature_index = i;
-    }
-  }
-  return `Lowest temperature was ${lowest_temperature} on ${weatherData[lowest_temperature_index].date} in city ${weatherData[lowest_temperature_index].city}, ${weatherData[lowest_temperature_index].country}.`;
+function lowestTemperature(weatherData) {
+  let lowest = weatherData.reduce((min, item) =>
+    item.minTemp < min.minTemp ? item : min
+  );
+  return `Lowest temperature was ${lowest.minTemp} on ${lowest.date} in city ${lowest.city}, ${lowest.country}.`;
 }
 
-function sorting_hottest_to_coolest(weatherData, totalData) {
-  for (let i = 0; i < totalData; i++) {
-    for (let j = 1; j < totalData - i; j++) {
-      if (weatherData[i].maxTemp < weatherData[j].maxTemp) {
-        let temp = weatherData[j];
-        weatherData[j] = weatherData[i];
-        weatherData[i] = temp;
-      }
-    }
-    return weatherData;
-  }
+function sortingHottestToCoolest(weatherData) {
+  return weatherData.sort((a, b) => b.maxTemp - a.maxTemp);
 }
 
 const weatherData = [
@@ -71,13 +49,13 @@ const weatherData = [
     country: "Pakistan",
   },
 ];
-// const weatherData = require("./weather_data.json");
+
 let totalData = weatherData.length;
 
-console.log(average_humidity(weatherData, totalData));
-console.log(highest_temperature(weatherData, totalData));
-console.log(lowest_temperature(weatherData, totalData));
+console.log(averageHumidity(weatherData, totalData));
+console.log(highestTemperature(weatherData));
+console.log(lowestTemperature(weatherData));
 console.log(
-  "Sorted data from hotest to coolest: ",
-  sorting_hottest_to_coolest(weatherData, totalData)
+  "Sorted data from hottest to coolest: ",
+  sortingHottestToCoolest(weatherData)
 );
